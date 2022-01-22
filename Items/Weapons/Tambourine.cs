d9782a9ebe80dd5a9150872bb8fccc032fa26bb5
Hydro <Jamison.Maloney100@outlook.com>
2022-01-21 20:33:29 -0600

    Tambourine is real now. Great for still targets, but poor against fast, moving targets.

--- Items/Weapons/Tambourine.cs
+++ Items/Weapons/Tambourine.cs
@@ -5,10 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
 using Terraria;
+using Terraria.ID;
 
 namespace Acceleration.Items.Weapons
 {
 	class Tambourine : ModItem
 	{
-	}
+        public override void SetDefaults()
+        {
+			item.damage = 10;
+			item.melee = true;
+			item.width = 48;
+			item.height = 48;
+			item.useTime = 25;
+			item.useAnimation = 25;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.noMelee = true;
+			item.knockBack = 0;
+			item.value = Item.sellPrice(silver: 50);
+			item.rare = ItemRarityID.Orange;
+			item.UseSound = Acceleration.BeamRifleSound;
+			item.shoot = ModContent.ProjectileType<Projectiles.Tambourine>();
+			item.shootSpeed = 10f;
+		}
+    }
 }
--- Projectiles/Tambourine.cs
+++ Projectiles/Tambourine.cs
@@ -5,10 +5,50 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace Acceleration.Projectiles
 {
 	class Tambourine : ModProjectile
-	{
-	}
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 48;
+            projectile.height = 48;
+            projectile.alpha = 0;
+            projectile.timeLeft = 360;
+            projectile.penetrate = -1;
+            projectile.hostile = false;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.melee = true;
+            projectile.light = 0.3f;
+        }
+        public override void AI()
+        {
+            base.AI();
+            // face our momentum
+            projectile.rotation += 25*Mathj.Matht.Deg2Rad;
+            Vector2 tamspeed = projectile.velocity;
+            tamspeed.Normalize();
+            if (projectile.ai[0] == 0 && (projectile.velocity.X * projectile.velocity.X) + (projectile.velocity.Y * projectile.velocity.Y) > 16f) //tambo slows down over time 
+            {
+                projectile.velocity -= 0.2f * tamspeed;
+            }
+            if (projectile.ai[0] == 0 && (projectile.velocity.X * projectile.velocity.X) + (projectile.velocity.Y * projectile.velocity.Y) < 16f) //slow down the slow down for some juicy boss dps 
+            {
+                projectile.velocity -= 0.05f * tamspeed;
+            }
+            if (projectile.ai[0] == 1) //tambo return 
+            {
+                projectile.velocity -= -0.1f * tamspeed;
+                projectile.tileCollide = true; 
+            }
+            if (Vector2.Dot(projectile.velocity, tamspeed) < 0) //dot product multiplies 
+            {
+                projectile.ai[0] = 1;
+            }
+        }
+    }
 }
