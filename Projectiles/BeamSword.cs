5b7d0252eebd0d25f4f2faf85d7ca4e76dbcd5a8
Yackerw <flippystar@gmail.com>
2022-01-24 03:10:56 -0500

    Sprites

--- Projectiles/BeamSword.cs
+++ Projectiles/BeamSword.cs
@@ -34,15 +34,22 @@ namespace Acceleration.Projectiles
         {
             // stick to the player
             Vector2 playerPos = Main.player[projectile.owner].position;
+            projectile.spriteDirection = (int)projectile.ai[1];
             switch (projectile.ai[0])
 			{
                 case 0:
                     projectile.rotation = Matht.Lerp(80 * Matht.Deg2Rad, 10 * Matht.Deg2Rad, (float)Math.Sin((projectile.timeLeft / 40.0f) * (Math.PI / 2)));
-                    Vector2 rotPos = new Vector2(35, -35).RotatedBy(projectile.rotation);
-                    projectile.position = playerPos + rotPos + new Vector2(-4, 0);
+                    Vector2 rotPos = new Vector2(20 * projectile.spriteDirection, -20).RotatedBy(projectile.rotation);
+                    projectile.position = playerPos + rotPos + new Vector2(20 * projectile.spriteDirection, 0);
                     break;
 			}
             ++animTimer;
         }
-    }
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+            AccelerationHelper.DrawSprite("Projectiles/BeamSword", projectile.position, 0, 48, Color.White, projectile.rotation, spriteBatch);
+            return false;
+		}
+	}
 }
