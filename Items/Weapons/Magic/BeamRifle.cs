398598a694eef7b9dd88f509cabf8e463c6d2b87
Yackerw <flippystar@gmail.com>
2022-01-25 14:37:49 -0500

    Removed accidental debug text

--- Items/Weapons/Magic/BeamRifle.cs
+++ Items/Weapons/Magic/BeamRifle.cs
@@ -109,7 +109,6 @@ namespace Acceleration.Items.Weapons.Magic
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
-			Main.NewText(hyper);
 			// dirty hack to prevent charge shot firing something
 			if (chargeFire)
 			{
